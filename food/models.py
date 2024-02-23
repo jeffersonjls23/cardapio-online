9f1f2aa5815612cfbc988e92c19d99a44fac6900
Nagi0 <brunolefonseca13@gmail.com>
2024-02-23 01:48:10 -0300

    fix: fix superuser

--- food/models.py
+++ food/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 CATEGORIES_LIST = (
@@ -19,3 +20,7 @@ class Food(models.Model):
     
     def __str__(self):
         return self.name
+
+
+class User(AbstractUser):
+    nick_name = models.CharField(default='nickname', max_length=20)
