648962680d08eb17fd31993be1dc1159b372e9dd
Nagi0 <brunolefonseca13@gmail.com>
2024-02-23 01:53:50 -0300

    fix: fix superuser

--- food/apps.py
+++ food/apps.py
@@ -1,17 +1,6 @@
-import os
-from .models import User
 from django.apps import AppConfig
 
 
 class FoodConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'food'
-
-    def ready(self):
-        email = os.getenv("EMAIL_ADMIN")
-        password = os.getenv("SENHA_ADMIN")
-
-        user = User.objects.filter(email=email)
-        if not user:
-            User.objects.create_superuser(username='admin', email=email, password=password,
-                                          is_active=True, is_staff=True)
