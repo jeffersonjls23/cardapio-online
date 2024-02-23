fcc589394853ac8de4f9fa820770da0611d1789c
Nagi0 <brunolefonseca13@gmail.com>
2024-02-23 01:31:23 -0300

    fix: fix settings

--- cardapio/settings.py
+++ cardapio/settings.py
@@ -32,7 +32,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["cardapio-online-production.up.railway.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
