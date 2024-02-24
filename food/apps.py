cdf0e6a163cb430b1a23f69f51eda2a8368b0e6a
Nagi0 <75706345+Nagi0@users.noreply.github.com>
2024-02-23 23:59:40 -0300

    feat: add user

--- food/apps.py
+++ food/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+from .models import Usuario
+import os
 
 
 class FoodConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'food'
+
+    def ready(self):
+        user = os.getenv("USER_ADMIN")
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
+
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios:
+            Usuario.objects.create_superuser(username=user, email=email, password=senha,
+                                             is_active=True, is_staff=True)
