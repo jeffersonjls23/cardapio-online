df2176c085cdb771a8dbf79a3b40bfeedc318d02
Nagi0 <75706345+Nagi0@users.noreply.github.com>
2024-02-24 08:15:01 -0300

    fix: run just front-end

--- food/tests.py
+++ food/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Usuario
+import django
+
 
 # Create your tests here.
+def create_su():
+    django.setup()
+    admin_email = "admin@email.com"
+    admin_user = 'admin'
+    admin_senha = 'admin'
+
+    usuarios = Usuario.objects.filter(email=admin_email)
+    if not usuarios:
+        Usuario.objects.create_superuser(username=admin_user, email=admin_email, password=admin_senha,
+                                         is_active=True, is_staff=True)
+    usuarios = Usuario.objects.filter(email=admin_email)
+    print(usuarios)
